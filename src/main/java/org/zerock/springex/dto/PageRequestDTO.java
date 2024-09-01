4612a20f5be20d0c2c384bfc6fa7801476f96cda
idaejin <ldj4241@naver.com>
2024-09-01 23:53:15 +0900

    요청시 검색 조건까지 포함

--- src/main/java/org/zerock/springex/dto/PageRequestDTO.java
+++ src/main/java/org/zerock/springex/dto/PageRequestDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
+import java.time.LocalDate;
 
 @Builder
 @Data
@@ -26,7 +27,31 @@ public class PageRequestDTO {
     @Positive
     private int size = 10;
 
+    private String link;
+
+    private String[] types;
+
+    private String keyword;
+
+    private boolean finished;
+
+    private LocalDate from;
+
+    private LocalDate to;
+
     public int getSkip(){
         return (page - 1) * 10;
+    }
+
+    public String getLink(){
+        if(link == null)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.append("page=" + this.page);
+            builder.append("&size=" + this.size);
+            link = builder.toString();
+        }
+
+        return link;
     }
 }
